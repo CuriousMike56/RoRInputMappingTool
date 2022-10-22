19ff1c4e02ebf19b62a609e2106c6a5b9f8ff996
Michael <46073351+CuriousMike56@users.noreply.github.com>
2022-10-22 00:00:38 -0400

    Updated Run.bat directory

--- bin/Release/Run.bat
+++ bin/Release/Run.bat
@@ -2,7 +2,7 @@
 :sub_setup
 TITLE Rigs of Rods Input Mapping Tool
 echo -- Rigs of Rods Input Mapping Tool --
-echo Copying Rigs of Rods 0.4 files to config folder
-xcopy  "%userprofile%\Documents\Rigs of Rods 0.4\config\input.map" "config" /s /e /y
+echo Copying Documents\My Games\Rigs of Rods files to config folder
+xcopy  "%userprofile%\Documents\My Games\Rigs of Rods\config\input.map" "config" /s /e /y
 echo Starting tool..(Do not close this window!)
 start "" /wait RoRInputMappingTool.exe
